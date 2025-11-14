aa1536c2312ad8848929a91f158c1035e3c95233
dznite <122264113+dznite@users.noreply.github.com>
2025-11-14 08:49:44 +0800

    Set new grid number
    Changed loop so that starting grid starts as 32x32.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-for (i=0;i<256;i++){
+for (i=0;i<1024;i++){
     let box = document.createElement('div');
     box.classList.add('boxes');
-    box.textContent = `${i}`;
+    // box.textContent = `${i}`;
     box.id = `box${i}`;
     document.querySelector('#container').appendChild(box);
 }
@@ -11,8 +11,12 @@ boxes.forEach((box)=>{
     box.addEventListener("mouseover", event => {
         if(event.buttons === 1){
             event.target.classList.add("hovered");
-            event.target.textContent = "";
+            // event.target.textContent = "";
         }
     })
     }
 )
+const sizeBtn = document.querySelector('#resize');
+sizeBtn.addEventListener("click",()=>{
+    let desiredBoxAmt = Number(prompt("Please indicate the desired amount of boxes per side"));
+})
