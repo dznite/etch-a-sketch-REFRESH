c58bd80390fffff9608cd7d08baa438ca8332f11
dznite <122264113+dznite@users.noreply.github.com>
2025-11-14 13:06:41 +0800

    Make change button functional
    Insert prompt that allows for user to input desired amount of boxes (max 100),
    along with safety conditions preventing them from inserting negative, null
    or non-numerical values.
    
    Also insert removeChild code to remove default grid and reused the
    same code to recreate a new grid, make it sketchable and have the
    the width and height dimensions of the boxes scale accordingly to the
    size of the container and the number of boxes.

--- script.js
+++ script.js
@@ -6,17 +6,52 @@ for (i=0;i<1024;i++){
     document.querySelector('#container').appendChild(box);
 }
 
-let boxes = document.querySelectorAll('.boxes');
+const boxes = document.querySelectorAll('.boxes');
 boxes.forEach((box)=>{
     box.addEventListener("mouseover", event => {
         if(event.buttons === 1){
             event.target.classList.add("hovered");
             // event.target.textContent = "";
         }
-    })
-    }
+    })}
 )
 const sizeBtn = document.querySelector('#resize');
 sizeBtn.addEventListener("click",()=>{
-    let desiredBoxAmt = Number(prompt("Please indicate the desired amount of boxes per side"));
-})
+    let inputBoxAmt = (prompt("Please indicate the desired amount of boxes per side"));
+    let desiredBoxAmt = Number(inputBoxAmt);
+    if (desiredBoxAmt <= 0 || desiredBoxAmt > 100 ){window.alert("Please enter a POSITIVE number from 1 - 100")}
+    else if (Number.isNaN(desiredBoxAmt)){window.alert("Please enter a NUMERICAL value from 1 -100")}
+    else if (!Number.isInteger(desiredBoxAmt)){window.alert("Please enter a WHOLE NUMBER from 1 - 100")} 
+    else {
+        let numDisplay = document.querySelector('#value');
+        numDisplay.innerText = desiredBoxAmt;
+
+        let originalGrid = document.querySelector("#container");
+        while (originalGrid.firstElementChild){
+            originalGrid.removeChild(originalGrid.firstElementChild);
+        };
+
+       for (i=0;i<(desiredBoxAmt*desiredBoxAmt);i++){
+        let box = document.createElement('div');
+        box.classList.add('boxes');
+        // box.textContent = `${i}`;
+        box.id = `box${i}`;
+        document.querySelector('#container').appendChild(box);
+        }
+
+        const boxes = document.querySelectorAll('.boxes');
+        boxes.forEach((box)=>{
+        box.addEventListener("mouseover", event => {
+        if(event.buttons === 1){
+            event.target.classList.add("hovered");
+            // event.target.textContent = "";
+        }
+            })}
+        )
+        
+        boxes.forEach((box)=>{
+            box.style.width = "calc(100%/" + desiredBoxAmt + ")";
+            box.style.height = "calc(100%/" + desiredBoxAmt + ")";
+        })
+    
+    }})
